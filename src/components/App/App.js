c3be9518cc43a0dd977ee7ecce0e210318f07401
hanjasn <hanjasn@gmail.com>
2021-05-01 19:08:07 -0700

    add styling to force user to enter non-empty values; enable/disable submit based on whether all information has been filled in

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,20 +11,25 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      initialInvestment: 0,
       contribution: 0,
-      annualRate: 0,
+      contributionRate: "monthly", // monthly, yearly
       years: 0,
-      result: 0
+      annualRate: 0,
+      result: 0,
     };
+    this.handleChangeInitialInvestment = this.handleChangeInitialInvestment.bind(this);
     this.handleChangeContribution = this.handleChangeContribution.bind(this);
-    this.handleChangeAnnualRate = this.handleChangeAnnualRate.bind(this);
+    this.handleChangeContributionRate = this.handleChangeContributionRate.bind(this);
     this.handleChangeYears = this.handleChangeYears.bind(this);
+    this.handleChangeAnnualRate = this.handleChangeAnnualRate.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleEnter = this.handleEnter.bind(this);
   }
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleEnter);
+  handleChangeInitialInvestment(e) {
+    this.setState({
+      initialInvestment: parseFloat(e.target.value)
+    });
   }
 
   handleChangeContribution(e) {
@@ -33,38 +38,44 @@ class App extends React.Component {
     });
   }
 
+  handleChangeContributionRate(e) {
+    this.setState({
+      contributionRate: e.target.value
+    });
+  }
+
+  handleChangeYears(e) {
+    this.setState({
+        years: parseInt(e.target.value)
+      });
+  }
+
   handleChangeAnnualRate(e) {
     this.setState({
       annualRate: parseFloat(e.target.value)
     });
   }
 
-  handleChangeYears(e) {
-    this.setState({
-      years: parseInt(e.target.value)
-    });
-  }
-
   handleSubmit() {
-    let calculation = 0;
+    let calculation = this.state.initialInvestment;
+    let contribution = null;
+    if (this.state.contributionRate === "yearly") {
+      contribution = this.state.contribution;
+    } else if (this.state.contributionRate === "monthly") {
+      contribution = 12 * this.state.contribution;
+    }
     for (let i = 0; i < this.state.years; ++i) {
-      calculation += this.state.contribution;
+      calculation += contribution;
       calculation *= 1 + (this.state.annualRate / 100);
     }
     this.setState({
       result: calculation
     });
   }
-
-  handleEnter(e) {
-    if (e.keyCode === 13) {
-      this.handleSubmit();
-    }
-  }
   
   render() {
     return (
-      <Container className="main-container">
+      <Container className="app-container">
         <Row>
           <Col className="d-flex justify-content-center">
             <h1>Interest Calculator</h1>
@@ -72,7 +83,9 @@ class App extends React.Component {
         </Row>
         <Row id="calc-form">
           <Col className="d-flex justify-content-center">
-            <CalcForm handleChangeContribution={this.handleChangeContribution} handleChangeAnnualRate={this.handleChangeAnnualRate} handleChangeYears={this.handleChangeYears} handleSubmit={this.handleSubmit} />
+            <CalcForm state={this.state} handleChangeInitialInvestment={this.handleChangeInitialInvestment} handleChangeContribution={this.handleChangeContribution} 
+            handleChangeContributionRate={this.handleChangeContributionRate} handleChangeAnnualRate={this.handleChangeAnnualRate} 
+            handleChangeYears={this.handleChangeYears} handleSubmit={this.handleSubmit}/>
           </Col>
         </Row>
         <Row id="result">
@@ -85,11 +98,12 @@ class App extends React.Component {
   }
 }
 App.propTypes = {
+  initialInvestment: PropTypes.number,
   contribution: PropTypes.number,
-  annualRate: PropTypes.number,
+  contributionRate: PropTypes.string,
   years: PropTypes.number,
-  showResult: PropTypes.bool,
-  result: PropTypes.number
+  annualRate: PropTypes.number,
+  result: PropTypes.number,
 };
 
 export default App;
--- src/components/CalcForm/CalcForm.js
+++ src/components/CalcForm/CalcForm.js
@@ -1,48 +1,107 @@
+import React from 'react';
 import './CalcForm.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-const CalcForm = function(props) {
-  return (
-    <div>
-      <Row>
-        <Col>
-          <label for="contribution">Contribution</label>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <input onChange={props.handleChangeContribution} type="number" id="contribution" />
-        </Col>
-      </Row>
-      <Row className="input-label">
-        <Col>
-          <label for="annual-rate">Annual Interest Rate %</label>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <input onChange={props.handleChangeAnnualRate} type="number" id="annual-rate" />
-        </Col>
-      </Row>
-      <Row className="input-label">
-        <Col>
-          <label for="years">Number of Years</label>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <input onChange={props.handleChangeYears} type="number" id="years" />
-        </Col>
-      </Row>
-      <Row className="calculate-button">
-        <Col className="d-flex justify-content-center">
-          <Button onClick={props.handleSubmit} variant="dark">Calculate</Button>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+class CalcForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleSubmit() {
+    let canSubmit = !isNaN(this.props.state.initialInvestment) && !isNaN(this.props.state.contribution) && 
+                      !isNaN(this.props.state.years) && !isNaN(this.props.state.annualRate);
+    if (canSubmit) {
+      this.props.handleSubmit();
+    }
+  }
+
+  handleKeyPress(e) {
+    if (e.keyCode === 13) {
+      this.handleSubmit();
+    }
+  }
+
+  render() {
+    const parentState = this.props.state;
+    const initialInvestmentInputStyle = isNaN(parentState.initialInvestment) ? "empty-input" : "";
+    const contributionInputStyle = isNaN(parentState.contribution) ? "empty-input" : "";
+    const yearsInputStyle = isNaN(parentState.years) ? "empty-input" : "";
+    const annualRateInputStyle = isNaN(parentState.annualRate) ? "empty-input" : "";
+    const emptyInputText = (<div className="empty-input-text">Please enter a value</div>);
+    return (
+      <div>
+        <Row>
+          <Col>
+            <label for="initial-investment">Initial investment</label>
+          </Col>
+        </Row>
+        <Row>
+          <span>$</span>
+          <Col>
+            <input onChange={this.props.handleChangeInitialInvestment} value={parentState.initialInvestment} type="number" 
+            className={initialInvestmentInputStyle} id="initial-investment" />
+            {isNaN(parentState.initialInvestment) && emptyInputText}
+          </Col>
+        </Row>
+        <Row className="input-label">
+          <Col>
+            <label for="contribution">Contribution</label>
+          </Col>
+        </Row>
+        <Row>
+          <span>$</span>
+          <Col>
+            <input onChange={this.props.handleChangeContribution} value={parentState.contribution} type="number" 
+            className={contributionInputStyle} id="contribution" />
+            {isNaN(parentState.contribution) && emptyInputText}
+          </Col>
+          <Col>
+            <select onChange={this.props.handleChangeContributionRate}> {/*No name attribute because not using a form*/}
+              <option value="monthly">Monthly</option>
+              <option value="yearly">Yearly</option>
+            </select>
+          </Col>
+        </Row>
+        <Row className="input-label">
+          <Col>
+            <label for="years">Years invested</label>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <input onChange={this.props.handleChangeYears} value={parentState.years} type="number" className={yearsInputStyle} d="years" />
+            {isNaN(parentState.years) && emptyInputText}
+          </Col>
+        </Row>
+        <Row className="input-label">
+          <Col>
+            <label for="annual-rate">Annual interest rate</label>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <input onChange={this.props.handleChangeAnnualRate} value={parentState.annualRate} type="number" className={annualRateInputStyle} 
+            id="annual-rate" />
+            <span> %</span>
+            {isNaN(parentState.annualRate) && emptyInputText}
+          </Col>
+        </Row>
+        <Row className="calculate-button">
+          <Col>
+            <Button onClick={this.handleSubmit} variant="dark">Calculate</Button>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
 
 export default CalcForm;
